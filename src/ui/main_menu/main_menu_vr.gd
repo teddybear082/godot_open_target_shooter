1f08967dc1986a9dba550fd2abdcb3e69ae0d95a
teddybear082 <87204721+teddybear082@users.noreply.github.com>
2022-09-26 13:06:22 -0400

    Get main menu working in VR, try original avatar
    -Set up main menu for VR, hook required methods
    -Try using default character from flat screen game as VRIK avatar in level 1
    -Update automatedavatar.gd to address more avatar fringe cases as evidenced by default flat character model
    -TO DO: Implement left hand radial menu option now return back to main menu in addition to quick quit; replace 0 icon with Quit.
    -TO DO: maybe implement hand animations for original flat screen player character or just go back to readyplayerme model instead.
    -TO DO: have controls menu on VR main menu point to VR controls instead of regular game controls

--- src/ui/main_menu/main_menu_vr.gd
+++ src/ui/main_menu/main_menu_vr.gd
@@ -4,7 +4,7 @@ extends Spatial
 ### Signals ###
 signal change_scene_request(target_scene_res_path)
 
-onready var main_menu_viewport_scene = $main_menu_viewport.get_scene_instance_load_placeholder()
+onready var main_menu_viewport_scene = $main_menu_viewport.scene_node
 
 func _ready():
 	main_menu_viewport_scene.connect("change_scene_request", self, "change_scene_request_signal_received")
