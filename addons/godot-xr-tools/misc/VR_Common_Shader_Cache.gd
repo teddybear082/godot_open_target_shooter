025c5a26ac8e1c6508a9367d4d0ad509708291a9
teddybear082 <87204721+teddybear082@users.noreply.github.com>
2022-09-30 07:03:18 -0400

    Enhance performance with shader cache

--- addons/godot-xr-tools/misc/VR_Common_Shader_Cache.gd
+++ addons/godot-xr-tools/misc/VR_Common_Shader_Cache.gd
@@ -2,12 +2,14 @@ extends Spatial
 
 signal cooldown_finished
 
-var countdown = 2
+var countdown = 5
 
 # Called every frame. 'delta' is the elapsed time since the previous frame.
 func _process(delta):
 	countdown = countdown - 1
 	if countdown == 0:
 		visible = false
+		$Particles.emitting = false
+		$Particles2.emitting = false
 		set_process(false)
 		emit_signal("cooldown_finished")
